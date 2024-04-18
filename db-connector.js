7c351cbf1a89be5acc27f3c0ffc1776ecfebee6e
Dylan Heard <dylan.heard.business@gmail.com>
2024-04-18 04:15:33 -0700

    Updated Paths for package.json

--- db-connector.js
+++ db-connector.js
@@ -5,10 +5,11 @@ if(process.env.JAWSDB_URL) {
   pool = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   pool = mysql.createConnection({
-    host: 'localhost',
-    user: process.env.LOCAL_DB_USER,
-    password: process.env.LOCAL_DB_PASSWORD,
-    database: process.env.LOCAL_DB_DATABASE,
+    host: 'qbct6vwi8q648mrn.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    user: 'i9bu6klyaf9ude4i',
+    password: 'c1k19uiwp0jbgyb6',
+    port: 3306,
+    database: 'bl7zp8lw6hw1qaz7',
   });
 }
 
